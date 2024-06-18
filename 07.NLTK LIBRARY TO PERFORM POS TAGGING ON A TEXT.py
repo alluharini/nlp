2f46eb13a536dadc09d5ae37c30fdfd516ca3859
Harini Allu <120156889+alluharini@users.noreply.github.com>
2024-06-18 23:36:32 +0530

    Update 07.NLTK LIBRARY TO PERFORM POS TAGGING ON A TEXT.py

--- 07.NLTK LIBRARY TO PERFORM POS TAGGING ON A TEXT.py
+++ 07.NLTK LIBRARY TO PERFORM POS TAGGING ON A TEXT.py
@@ -1,19 +1,7 @@
 import nltk
-from nltk.tokenize import word_tokenize
-
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-
-def pos_tagging(text):
-    tokens = word_tokenize(text)
-    tagged_tokens = nltk.pos_tag(tokens)
-    return tagged_tokens
-
-def main():
-    text = "The quick brown fox jumps over the lazy dog"
-    tagged_text = pos_tagging(text)
-    print("POS tagged text:")
-    print(tagged_text)
-
-if __name__ == "__main__":
-    main()
+from nltk import word_tokenize, pos_tag
+text = "harini is a good girl."
+tokens = word_tokenize(text)
+pos_tags = pos_tag(tokens)
+print("POS Tagged Text:")
+print(pos_tags)
